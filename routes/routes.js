ddde65805fe1af84d06c171df55e7fae028d2bf9
Scott <schepers.scott@hotmail.com>
2021-11-22 16:54:42 +0100

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,9 +6,10 @@ const retrieveImage = require("./controllers/retrieveImage");
 const updateImage = require("./controllers/updateImage");
 const deleteImage = require("./controllers/deleteImage");
 
-router.get("/", (request, response, next) => {
-  response.json({ message: "Hey! This is your server response!" });
-  next();
+// Get Orders
+router.get("/", async (req, res) => {
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
 });
 
 // image upload API
@@ -25,5 +26,17 @@ router.delete("/delete-image/:cloudinary_id", deleteImage.deleteImage);
 
 // update image
 router.put("/update-image/:cloudinary_id", updateImage.updateImage);
+
+async function loadPostsCollection() {
+	const client = await mongodb.MongoClient.connect(
+		"mongodb+srv://user:user@automatedadventure.sriye.mongodb.net/AutomatedAdventure?retryWrites=true&w=majority",
+		{
+			useNewUrlParser: true,
+		}
+	);
+
+	db = client.db("current_stats");
+	return db.collection("playerstats");
+}
 
 module.exports = router;
