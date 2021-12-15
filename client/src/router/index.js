2111bc115370843395a4706fe098737003d1e92e
Scott <schepers.scott@hotmail.com>
2021-12-15 17:37:24 +0100

    vuetify working card

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import ToDo from "@/components/ToDo";
+import Player from "@/components/Player";
 
 Vue.use(Router);
 
@@ -8,7 +9,7 @@ export default new Router({
   routes: [
     {
       path: "/",
-      component: ToDo
+      component: Player
     }
   ]
 });
